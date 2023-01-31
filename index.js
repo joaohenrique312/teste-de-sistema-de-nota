ccac41ab17b19b1b816eb2fe1dd24391037c02e0
João Henrique <112218661+joaohenrique312@users.noreply.github.com>
2023-01-31 08:48:39 -0400

    index.js deletado
    Deletado por causa da minha aprimoração em .JS e levando em consideração que o .JS antigo não era perfeito em formato de site.

--- index.js
+++
@@ -1,7 +0,0 @@
-var nome = window.prompt('Nome do aluno')
-var n1 = Number (window.prompt('Nota da prova parcial'))
-var n2 = Number(window.prompt('Nota da prova trimestral'))
-var n3 = 2
-var soma = (n1 + n2) / n3
-var res = soma > 7?'aprovado':'reprovado'
-document.writeln('A média trimestral do '+nome ,' foi  '+ soma+ ' logo, ele foi ' + res)
