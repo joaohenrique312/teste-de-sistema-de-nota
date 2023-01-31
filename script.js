ec12125ac8a4285eec5729770fa25aab172b4b38
João Henrique <112218661+joaohenrique312@users.noreply.github.com>
2023-01-31 09:25:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function calcular(){
+    //Variáveis dos inputs do html
     var name = document.getElementById('nome').value
     var n1 = document.getElementById('np')
     var n2 = document.getElementById('nt')
     var n3 = document.getElementById('na')
     var nfres = document.getElementById('res')
 
-    if(n1,n2,n3.value == 0){
+    if(n1,n2,n3.value == 0){ //Nota não inserida
         window.alert('Digite os números nos inputs abaixo.')
         nfres.innerHTML = '<p>Nenhuma nota foi inserida.</p>'
-    } else{
+    } else{ //Números das variáveis de notas
         let p = Number(n1.value)
         let t = Number(n2.value)
         let a = Number(n3.value)
         let f = (p+t+a)/3
-        if(f<6){
+        if(f<6){ //Resultado final 
             nfres.innerHTML = `<p>O aluno ${name} foi reprovado.</p>`
         } else{
             nfres.innerHTML = `<p>O aluno ${name} foi aprovado! Boa semana de descanso.`
         }
     }
-    
-
 }
